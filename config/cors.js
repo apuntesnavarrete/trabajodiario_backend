a23f2162d5eed4c3199b15ff0358b0619d369ec2
apuntesnavarrete <apuntesnavarrete@gmail.com>
2025-11-05 16:20:25 -0600

    error de cors

--- config/cors.js
+++ config/cors.js
@@ -2,8 +2,8 @@ import cors from "cors";
 
 // Configuración CORS con cookies habilitadas
 export default cors({
-  origin: ["http://localhost:5175", "http://50.21.187.205/"], // tu frontend (Vite usa este puerto)
-  credentials: true,               // 👈 permite enviar y recibir cookies
+  origin: ["http://localhost:5175", "http://50.21.187.205"], // 👈 sin "/"
+  credentials: true, // permite cookies o encabezados con credenciales
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 });
