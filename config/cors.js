f0356a83a783512f73ac30232f7b27a608cce1b4
apuntesnavarrete <apuntesnavarrete@gmail.com>
2026-03-31 15:27:05 -0600

    cors por dotev

--- config/cors.js
+++ config/cors.js
@@ -1,8 +1,23 @@
 import cors from "cors";
+const frontendProd = process.env.FRONTEND_URL; 
+
+const allowedOrigins = [
+  frontendProd,
+  "http://localhost:5174"
+];
+
 
 // Configuración CORS con cookies habilitadas
 export default cors({
-  origin: ["http://localhost:5175", "http://50.21.187.205"], // 👈 sin "/"
+ origin: function (origin, callback) {
+    // Si no hay origin (Postman) o está en la lista, permitimos
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log("Origen bloqueado por CORS:", origin); // Útil para debug
+      callback(new Error("No permitido por CORS"));
+    }
+  },
   credentials: true, // permite cookies o encabezados con credenciales
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
